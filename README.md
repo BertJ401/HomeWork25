f0bee49504cf5e87e71968fa23869f4b10717990
BertJ401 <bertjohnson401@gmail.com>
2020-06-25 21:58:18 -0500

    Clean Up, Removed Comments, et.al.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Alright, Day Planner -
 I started by grabbing the date and breaking it down, using preset arrays to generate a user friendly Date, generate hour names outside of military time.
-I then dynamically generated the day. I went with CSS Grid straight up instead of bootstrap and hard coded that rather than appending it to every line via jQuery/javascript.
+I then dynamically generated the day. I went with CSS straight up instead of bootstrap and hard coded that rather than appending it to every line via jQuery/javascript.
 Apparently createElement is faster than jQuery methods, and after reading way too much of a forum post, I used that instead of jQuery, which dominates the rest of this project.
--- script.js
+++ script.js
@@ -6,11 +6,8 @@ var dayIndex = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
 var monthIndex = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 let containBox = document.querySelector(".container");
 
-
-//dynamically create our rows and readable times 
 $(document).ready(function () {
 
-    //get date/hour of the day
     var d = new Date();
     var dayTense = d.getHours();
     var dateNum = d.getDate();
@@ -22,12 +19,14 @@ $(document).ready(function () {
 
     for (i = 0; i < 9; i++) {
 
+        var hourRow = document.createElement("div");
+        var savedInfo = localStorage.getItem(i);
+        var hourDisplay = document.createElement("div");
+        var bodyDisplay = document.createElement("input");
+        var saveIt = document.createElement("btn");
         var hourTitle = i + 9;
         hourIndex[i] = parseFloat(hourTitle);
 
-
-        //set note field css based on time of day
-    
         var dayShade = "present"
         if (dayTense > hourTitle) {
             dayShade = "past";
@@ -35,7 +34,6 @@ $(document).ready(function () {
             dayShade = "future"
         };
 
-        //convert to am/pm
         amPm = "am";
         if (hourTitle > 12) {
             hourTitle = hourTitle - 12;
@@ -43,37 +41,23 @@ $(document).ready(function () {
         }
         hourName[i] = String(hourTitle) + amPm;
 
-        
-
-        var hourRow = document.createElement("div");
         $(hourRow).addClass("row time-block entryRow" + i);
         $(containBox).append(hourRow)
-        var hourDisplay = document.createElement("div");
         $(hourDisplay).addClass("hour end-block").text(hourName[i]);
         $(hourRow).append(hourDisplay);
-        var savedInfo = localStorage.getItem(i);
-        console.log(savedInfo);
-        var bodyDisplay = document.createElement("input");
-        $(bodyDisplay).attr("id","inputField"+i)
-        $(bodyDisplay).attr("placeholder",savedInfo);
+        $(bodyDisplay).attr("id", "inputField" + i)
+        $(bodyDisplay).attr("placeholder", savedInfo);
         $(bodyDisplay).addClass(dayShade + " mid-block").attr("type", "text");
         $(hourRow).append(bodyDisplay);
-        var saveIt = document.createElement("btn");
-        $(saveIt).addClass("saveBtn end-block").attr("data-ind",i).text("SAVE");
+        $(saveIt).addClass("saveBtn end-block").attr("data-ind", i).text("SAVE");
         $(hourRow).append(saveIt);
 
-
-
-        // console.log(hourTitle);
-        // console.log(amPm);
-        // console.log(hourName[i]);
     }
 
-    $(document).on("click", ".saveBtn", function(){
+    $(document).on("click", ".saveBtn", function () {
         var dataAtt = $(this).attr("data-ind");
-        var inField=$("#inputField"+dataAtt).val();
-        window.localStorage.setItem(dataAtt,inField);
-        console.log(dataAtt, inField);
+        var inField = $("#inputField" + dataAtt).val();
+        window.localStorage.setItem(dataAtt, inField);
     })
 
 
